43353ae994cae1e456c42205929c75671603946e
Raymundo1 <cquptcxl@gmail.com>
2019-03-08 10:14:38 -0700

    add licence

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
+# Modified work Copyright 2019 Xinlei Chen
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
